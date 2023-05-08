73f3462c6e458f8990aed0a0768018ba050c805d
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-08 20:48:53 +0900

    chore: 폰트 및 스크린 breakpoint 추가

--- client/to-do-list-app/tailwind.config.js
+++ client/to-do-list-app/tailwind.config.js
@@ -12,7 +12,35 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ["IM_Hyemin-Bold", "Arial", "sans-serif"], 
+        // sans가 제일 기본 상속 폰트이므로 전체 폰트바꾸려면 sans재지정후 맨앞에 원하는 폰트 넣기
+      
+        omyu: ["omyu_pretty", "sans-serif"], // 채팅
+        hye: ['IM_Hyemin-Bold', "sans-serif"] // 기본 컨셉
+      
+      }
     },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'mm': {'max': '807px'},
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
+
+
   },
   plugins: [],
 }
