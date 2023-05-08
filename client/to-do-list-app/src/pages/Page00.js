771e59f04892c9b8a0050455ab6b10377a2c044d
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-08 15:59:58 +0900

    feat: To-do-list 메인보드 생성

---
+++ client/to-do-list-app/src/pages/Page00.js
@@ -0,0 +1,17 @@
+import TodoItem from "./Todo"
+
+const Page00 = () => {
+    return (
+        <article className="font-hye ">
+            <div>
+                <h1>오늘의 할 일</h1>
+                <TodoItem />
+            </div>
+            <h2>Upcoming</h2>
+
+
+        </article>
+    )
+}
+
+export default Page00
