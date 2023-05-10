23adddf32cd61278a75173b162526f8431086c7b
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-10 23:01:40 +0900

    chore: colors 설정값 추가

--- client/to-do-list-app/tailwind.config.js
+++ client/to-do-list-app/tailwind.config.js
@@ -19,7 +19,11 @@ module.exports = {
         omyu: ["omyu_pretty", "sans-serif"], // 채팅
         hye: ['IM_Hyemin-Bold', "sans-serif"] // 기본 컨셉
       
+      },
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
       }
+      
     },
     screens: {
       '2xl': {'max': '1535px'},
