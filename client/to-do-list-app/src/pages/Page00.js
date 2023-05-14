ac081f35dc18c7f73047d15ffc0a6f2ce7e3961b
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-05-14 22:48:03 +0900

    feat: To-do-list 메인보드

--- client/to-do-list-app/src/pages/Page00.js
+++ client/to-do-list-app/src/pages/Page00.js
@@ -3,11 +3,17 @@ import TodoItem from "./Todo"
 const Page00 = () => {
     return (
         <article className="font-hye ">
-            <div>
-                <h1>오늘의 할 일</h1>
-                <TodoItem />
+            <div className="flex justify-between pr-10">
+                <h1 className="ml-10">오늘의 할 일</h1>
+                <div className="flex ">
+                    <h1 className="ml-10">삭제</h1>
+                    <h1 className="ml-10">모두 삭제</h1>
+                </div>
             </div>
-            <h2>Upcoming</h2>
+            <TodoItem />
+            <div className="ml-10"> 다가오는 일정 </div>
+            <TodoItem />
+            {/* <h2>Upcoming</h2> */}
 
 
         </article>
